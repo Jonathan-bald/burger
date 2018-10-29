7f9d9d2802989260792634b0c60c724282355930
Jonathan Bald <baldyj117@yahoo.com>
2018-10-29 17:50:18 -0500

    finished

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,1 +1,39 @@
+var express = require("express");
+var router = express.Router();
 
+var burger = require("../models/burger.js")
+
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log("all burgers: " + hbsObject);
+      res.render("index", hbsObject);
+    });
+});
+  
+router.post("/api/burgers", function(req, res) {
+    console.log("new burger: " + req.body.burger_name);
+    burger.insertOne([req.body.burger_name], function(result) {
+
+      res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {  
+    console.log("updating id: " + req.params.id);
+    burger.updateOne(
+      req.params.id, function(result) {
+        if (result.changedRows === 0) {
+
+          console.log("404");
+          return res.status(404).end();
+        }
+        res.status(200).end();
+  
+      }
+    );
+  });
+
+module.exports = router;
--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,11 @@
+DROP DATABASE if EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
 
+CREATE TABLE burgers
+(
+id int NOT NULL AUTO_INCREMENT,
+burger_name varchar(255) NOT NULL,
+devoured boolean DEFAULT 0,
+PRIMARY KEY (id)
+);
