a1ffe1b2777e32532931e20d33be85df2957b6f9
Jonathan Bald <38895283+baldyj117@users.noreply.github.com>
2018-10-29 17:45:10 -0500

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("bacon cheeseburger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("jalapeno burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("double cheeseburger");
