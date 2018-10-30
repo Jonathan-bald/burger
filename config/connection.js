d64f1048c97a9bae2e1b91499dd18f786a3f4fc9
Jonathan Bald <38895283+baldyj117@users.noreply.github.com>
2018-10-29 19:10:22 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,5 @@
 var mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
-	// DB is JawsDB on Heroku
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -20,6 +15,6 @@ connection.connect(function(err){
     }
     console.log("connected as id " + connection.threadID);
 });
-}
+
 
 module.exports = connection;
