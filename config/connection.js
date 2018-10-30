e44494f31b29455ae75f71a78ed534242438fedf
Jonathan Bald <baldyj117@yahoo.com>
2018-10-30 12:12:45 -0500

    final

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,17 @@
 var mysql = require("mysql");
+var connection;
 
-//Basic connetion setup
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-})
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    } else {
+        connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'root',
+            database: 'burgers_db'
+        });
+    };
 connection.connect((err) => {
     if (err){
         console.error("error while connecting: "+ err.stack);
@@ -19,4 +21,5 @@ connection.connect((err) => {
 })
 
 //exports the connetion for ORM
+connection.connection();
 module.exports = connection;
